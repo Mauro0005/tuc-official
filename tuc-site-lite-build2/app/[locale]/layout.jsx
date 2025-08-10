495a33e93b9674e51cd3ad57c5ed43d6cecaa191
Mauro0005 <mauro@euroslam.com>
2025-08-10 20:47:56 +0200

    Add files via upload

---
+++ tuc-site-lite-build2/app/[locale]/layout.jsx
@@ -0,0 +1,1 @@
+import './locale.css'; import Navbar from '../../components/Navbar'; import { ThemeProvider } from '../../components/ThemeProvider'; export default function LocaleLayout({children,params}){const {locale}=params; return(<ThemeProvider><Navbar locale={locale}/><main>{children}</main></ThemeProvider>);}
