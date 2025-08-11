d91d51e969f36a75269ce16a2c39efbe300b09a3
Mauro0005 <mauro@euroslam.com>
2025-08-11 23:37:59 +0200

    Add files via upload

---
+++ tuc-site-vibrant-style/app/layout.jsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import SiteBackground from "../components/SiteBackground";
+
+export const metadata = {
+  title: "Torneo Urbis Cruoninga",
+  description: "Official outdoor 5v5 tournament in Groningen",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="nl" suppressHydrationWarning>
+      <body className="body-wrap">
+        <SiteBackground />
+        {children}
+      </body>
+    </html>
+  );
+}
