01390071d7a0a002cb7aa3c2cbe84e640f0baabd
Mauro0005 <mauro@euroslam.com>
2025-08-11 18:27:13 +0200

    Delete tuc-site-lite-build2-mobile-friendly-fixed directory

--- tuc-site-lite-build2-mobile-friendly-fixed/app/[locale]/layout.jsx
+++
@@ -1,1 +0,0 @@
-import './locale.css'; import Navbar from '../../components/Navbar'; import { ThemeProvider } from '../../components/ThemeProvider'; export default function LocaleLayout({children,params}){const {locale}=params; return(<ThemeProvider><Navbar locale={locale}/><main>{children}</main></ThemeProvider>);}
