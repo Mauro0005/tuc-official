b2d5309a6431cdd25faeefe714084e139fad0ea1
Mauro0005 <mauro@euroslam.com>
2025-08-11 21:53:37 +0200

    Delete tuc-site-lite-navbar-desktop-one-row directory

--- tuc-site-lite-navbar-desktop-one-row/app/[locale]/layout.jsx
+++
@@ -1,6 +0,0 @@
-import Script from "next/script";
-import SiteBackground from "../../components/SiteBackground";
-import "../../styles/enhance.css";
-import './locale.css'; import Navbar from '../../components/Navbar'; import { ThemeProvider } from '../../components/ThemeProvider'; export default function LocaleLayout({children,params}){const {locale}=params; return(<ThemeProvider><Navbar locale={locale}/>
-        <SiteBackground />
-      <div className="h-2 sm:h-0" /><main>{children}</main></ThemeProvider>);}
